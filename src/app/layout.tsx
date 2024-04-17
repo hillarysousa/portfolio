b04d632bee6653dac1a99466b26571eb04b79932
Hillary Sousa <hillaryhpf@gmail.com>
2024-04-16 22:34:40 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Hillary Sousa - Design & Development",
+  description: "Portfólio de Hillary Sousa",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
