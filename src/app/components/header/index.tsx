1c4b224cfa5c6191244d714c73a7dcc6e3451dd0
Hillary Sousa <hillaryhpf@gmail.com>
2024-04-16 22:34:40 -0300

    initial commit

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <menu>
+            <li>home</li>
+            <li>portfólio</li>
+            <li>experiência</li>
+            <li>contato</li>
+        </menu>
+    )
+}
