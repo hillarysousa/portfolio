a6221b05f366c0c18ffdbf93b196f5d5910d8446
Hillary Sousa <hillaryhpf@gmail.com>
2024-04-18 00:38:02 -0300

    creating header skeleton and adding css grid styling

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
+
 export default function Header() {
-    return (
-        <menu>
-            <li>home</li>
-            <li>portfólio</li>
-            <li>experiência</li>
-            <li>contato</li>
-        </menu>
-    )
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src="https://placehold.co/125" alt="Hillary Sousa" width={125} height={125} />
+      </div>
+      <menu className="header__menu">
+        <li className="active">home</li>
+        <li>portfólio</li>
+        <li>experiência</li>
+        <li>contato</li>
+        <li>lang</li>
+      </menu>
+    </header>
+  )
 }
