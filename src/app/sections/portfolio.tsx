9cf883422d2641ba2678df0b972771759cf0b4e2
Hillary Sousa <hillaryhpf@gmail.com>
2024-04-19 02:02:25 -0300

    finished home view

---
+++ src/app/sections/portfolio.tsx
@@ -0,0 +1,8 @@
+export default function Portfolio() {
+    return (
+        <section id="portfolio" className="portfolio">
+            <h2 className="portfolio__title">portfólio</h2>
+            <p className="portfolio__text">alguns dos meus projetos mais recentes</p>
+        </section>
+    )
+}
