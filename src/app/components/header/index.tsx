7680968f1479017b9a72d4785850a8ab3556f2f5
Hillary Sousa <hillaryhpf@gmail.com>
2024-04-25 03:23:32 -0300

    adding scroll anchor effects

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,21 +1,44 @@
+'use client';
 import Image from "next/image";
+import { useHash } from "@/app/hooks";
 import Logo from "../../assets/images/logo.png";
-import globe from "../../assets/svg/globe.svg";
 
 import './style.scss';
+import { useEffect } from "react";
 
 export default function Header() {
+  console.log(window.location.hash, 'pathname >>>')
+  const [hash, setHash] = useHash();
+
+  const handleClick = (anchor: string) => {
+    return setHash(anchor);
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section[id]');
+      sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top > 0 && rect.top < 200) {
+          if (!hash.length || hash !== window.location.hash) {
+            history.replaceState(null, '', `#${section.id}`);
+          }
+        }
+      });
+    }
+    document.addEventListener('scroll', handleScroll);
+  });
+
+  console.log(window.location.hash, 'hash >>>')
   return (
     <header className="header">
       <div className="header__logo">
         <Image src={Logo} alt="Hillary Sousa" />
       </div>
       <menu className="header__menu">
-        <li className="active"><a href="#home">home</a></li>
-        {/* <li><a href="#portfolio">portfólio</a></li> */}
-        <li><a href="#experience">experiência</a></li>
-        <li><a href="#contact">contato</a></li>
-        {/* <li><Image src={globe} alt="mude o idioma" className="header__menu__icon" /></li> */}
+        <li className={window.location.hash === '#home' || window.location.hash === '' ? 'active' : ''}><a onClick={() => handleClick('#home')}>home</a></li>
+        <li className={window.location.hash === '#experience' ? 'active' : ''}><a onClick={() => handleClick('#experience')}>experiência</a></li>
+        <li className={window.location.hash === '#contact' ? 'active' : ''}><a onClick={() => handleClick('#contact')}>contato</a></li>
       </menu>
     </header>
   )
