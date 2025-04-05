20b2cf50bb091e4491076606ef0efcd9c92a514c
Hillary Sousa <hillaryhpf@gmail.com>
2025-04-05 20:04:20 -0300

    add new job to experience section - closes #9

--- src/app/[locale]/components/experience-tabs/index.tsx
+++ src/app/[locale]/components/experience-tabs/index.tsx
@@ -1,59 +1,95 @@
-'use client'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { useTranslation } from 'react-i18next';
+"use client";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { useTranslation } from "react-i18next";
 
-import './style.scss';
+import "./style.scss";
 
 export default function ExperienceTabs() {
-    const { t } = useTranslation('t');
+  const { t } = useTranslation("t");
 
-    return (
-        <Tabs className="tabs" selectedTabClassName="tabs__tab-selected" >
-            <TabList className="tabs__list">
-                <Tab className="tabs__tab">{t("t:experience:tabs:first_tab:title")}</Tab>
-                <Tab className="tabs__tab">{t("t:experience:tabs:second_tab:title")}</Tab>
-                <Tab className="tabs__tab">{t("t:experience:tabs:third_tab:title")}</Tab>
-            </TabList>
+  return (
+    <Tabs className="tabs" selectedTabClassName="tabs__tab-selected">
+      <TabList className="tabs__list">
+        <Tab className="tabs__tab">
+          {t("t:experience:tabs:first_tab:title")}
+        </Tab>
+        <Tab className="tabs__tab">
+          {t("t:experience:tabs:second_tab:title")}
+        </Tab>
+        <Tab className="tabs__tab">
+          {t("t:experience:tabs:third_tab:title")}
+        </Tab>
+        <Tab className="tabs__tab">
+          {t("t:experience:tabs:fourth_tab:title")}
+        </Tab>
+      </TabList>
 
-            <TabPanel className="tabs__tabpanel">
-                <h3 className='tabs__tabpanel__title'>{t("t:experience:tabs:first_tab:subtitle")}</h3>
-                <p className='tabs__tabpanel__period'>{t("t:experience:tabs:first_tab:period")}</p>
-                <div className='tabs__tabpanel__content'>
-                    <ul>
-                        <li>{t("t:experience:tabs:first_tab:topics:first")}</li>
-                        <li>{t("t:experience:tabs:first_tab:topics:second")}.</li>
-                        <li>{t("t:experience:tabs:first_tab:topics:third")}</li>
-                        <li>{t("t:experience:tabs:first_tab:topics:fourth")}</li>
-                    </ul>
-                </div>
-            </TabPanel>
+      <TabPanel className="tabs__tabpanel">
+        <h3 className="tabs__tabpanel__title">
+          {t("t:experience:tabs:first_tab:subtitle")}
+        </h3>
+        <p className="tabs__tabpanel__period">
+          {t("t:experience:tabs:first_tab:period")}
+        </p>
+        <div className="tabs__tabpanel__content">
+          <ul>
+            <li>{t("t:experience:tabs:first_tab:topics:first")}</li>
+            <li>{t("t:experience:tabs:first_tab:topics:second")}.</li>
+            <li>{t("t:experience:tabs:first_tab:topics:third")}</li>
+          </ul>
+        </div>
+      </TabPanel>
 
-            <TabPanel className="tabs__tabpanel">
-                <h3 className='tabs__tabpanel__title'>{t("t:experience:tabs:second_tab:subtitle")}</h3>
-                <p className='tabs__tabpanel__period'>{t("t:experience:tabs:second_tab:period")}</p>
-                <div className='tabs__tabpanel__content'>
-                    <ul>
-                        <li>{t("t:experience:tabs:second_tab:topics:first")}</li>
-                        <li>{t("t:experience:tabs:second_tab:topics:second")}.</li>
-                        <li>{t("t:experience:tabs:second_tab:topics:third")}</li>
-                        <li>{t("t:experience:tabs:second_tab:topics:fourth")}</li>
-                    </ul>
-                </div>
-            </TabPanel>
+      <TabPanel className="tabs__tabpanel">
+        <h3 className="tabs__tabpanel__title">
+          {t("t:experience:tabs:second_tab:subtitle")}
+        </h3>
+        <p className="tabs__tabpanel__period">
+          {t("t:experience:tabs:second_tab:period")}
+        </p>
+        <div className="tabs__tabpanel__content">
+          <ul>
+            <li>{t("t:experience:tabs:second_tab:topics:first")}</li>
+            <li>{t("t:experience:tabs:second_tab:topics:second")}.</li>
+            <li>{t("t:experience:tabs:second_tab:topics:third")}</li>
+            <li>{t("t:experience:tabs:second_tab:topics:fourth")}</li>
+          </ul>
+        </div>
+      </TabPanel>
 
-            <TabPanel className="tabs__tabpanel">
-                <h3 className='tabs__tabpanel__title'>{t("t:experience:tabs:third_tab:subtitle")}</h3>
-                <p className='tabs__tabpanel__period'>{t("t:experience:tabs:third_tab:period")}</p>
-                <div className='tabs__tabpanel__content'>
-                    <ul>
-                        <li>{t("t:experience:tabs:third_tab:topics:first")}</li>
-                        <li>{t("t:experience:tabs:third_tab:topics:second")}.</li>
-                        <li>{t("t:experience:tabs:third_tab:topics:third")}</li>
-                        <li>{t("t:experience:tabs:third_tab:topics:fourth")}</li>
-                    </ul>
-                </div>
-            </TabPanel>
+      <TabPanel className="tabs__tabpanel">
+        <h3 className="tabs__tabpanel__title">
+          {t("t:experience:tabs:third_tab:subtitle")}
+        </h3>
+        <p className="tabs__tabpanel__period">
+          {t("t:experience:tabs:third_tab:period")}
+        </p>
+        <div className="tabs__tabpanel__content">
+          <ul>
+            <li>{t("t:experience:tabs:third_tab:topics:first")}</li>
+            <li>{t("t:experience:tabs:third_tab:topics:second")}.</li>
+            <li>{t("t:experience:tabs:third_tab:topics:third")}</li>
+            <li>{t("t:experience:tabs:third_tab:topics:fourth")}</li>
+          </ul>
+        </div>
+      </TabPanel>
 
-        </Tabs>
-    )
+      <TabPanel className="tabs__tabpanel">
+        <h3 className="tabs__tabpanel__title">
+          {t("t:experience:tabs:fourth_tab:subtitle")}
+        </h3>
+        <p className="tabs__tabpanel__period">
+          {t("t:experience:tabs:fourth_tab:period")}
+        </p>
+        <div className="tabs__tabpanel__content">
+          <ul>
+            <li>{t("t:experience:tabs:fourth_tab:topics:first")}</li>
+            <li>{t("t:experience:tabs:fourth_tab:topics:second")}.</li>
+            <li>{t("t:experience:tabs:fourth_tab:topics:third")}</li>
+            <li>{t("t:experience:tabs:fourth_tab:topics:fourth")}</li>
+          </ul>
+        </div>
+      </TabPanel>
+    </Tabs>
+  );
 }
