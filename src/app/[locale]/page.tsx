3a07c9b76198cefd0051a065a3995b19f7f190f0
Hillary Sousa <hillaryhpf@gmail.com>
2025-05-31 03:49:39 -0300

    adding back the portfolio section along with improvements and a new project

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import Home from "./sections/home";
 import Portfolio from "./sections/portfolio";
 import Experience from "./sections/experience";
@@ -6,24 +6,30 @@ import Contact from "./sections/contact";
 import TranslationsProvider from "./providers/translations";
 import initTranslations from "../i18n";
 
-const i18nNamespaces = ['t'];
+const i18nNamespaces = ["t"];
 
-const NoSSRHeader = dynamic(() => import('./components/header'), { ssr: false })
+const NoSSRHeader = dynamic(() => import("./components/header"), {
+  ssr: false,
+});
 
-
-export default async function App({ params: { locale } }: { params: { locale: string } }) {
+export default async function App({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
   const { resources } = await initTranslations(locale, i18nNamespaces);
   return (
     <TranslationsProvider
       namespaces={i18nNamespaces}
       locale={locale}
-      resources={resources}>
+      resources={resources}
+    >
       <NoSSRHeader />
       <main>
-        <Home params={{ locale }} />
-        {/* <Portfolio /> */}
-        <Experience params={{ locale }} />
-        <Contact params={{ locale }} />
+        <Home />
+        <Portfolio />
+        <Experience />
+        <Contact />
       </main>
     </TranslationsProvider>
   );
--- src/app/[locale]/sections/contact.tsx
+++ src/app/[locale]/sections/contact.tsx
@@ -1,42 +1,86 @@
+"use client";
 import Image from "next/image";
-import initTranslations from "@/app/i18n";
+import { useTranslation } from "react-i18next";
 import Behance from "../assets/svg/behance.svg";
 import GitHub from "../assets/svg/github.svg";
 import LinkedIn from "../assets/svg/linkedin.svg";
 import Mail from "../assets/svg/mail.svg";
 
-export default async function Contact({ params: { locale } }: { params: { locale: string } }) {
+export default function Contact() {
+  const { t } = useTranslation("t");
+  const currentDate = new Date();
 
-    const { t } = await initTranslations(locale, ['t']);
+  const currentYear = currentDate.getFullYear();
 
-    const currentDate = new Date;
-
-    const currentYear = currentDate.getFullYear();
-
-    return (
-        <section id="contact" className="contact">
-            <div className="contact__textarea">
-                <h2 className="contact__textarea__title">{t("t:contact:title")}</h2>
-                <ul className="contact__textarea__socials">
-                    <li><a href="https://www.linkedin.com/in/hillarysousa/" target="_blank" className="contact__textarea__socials__link">
-                        <Image src={LinkedIn} alt="LinkedIn" className="contact__textarea__socials__link__icon" /> hillarysousa
-                    </a></li>
-                    <li><a href="https://github.com/hillarysousa" target="_blank" className="contact__textarea__socials__link">
-                        <Image src={GitHub} alt="GitHub" className="contact__textarea__socials__link__icon" /> hillarysousa
-                    </a></li>
-                    <li><a href="https://www.behance.net/hillarysousa" target="_blank" className="contact__textarea__socials__link">
-                        <Image src={Behance} alt="Behance" className="contact__textarea__socials__link__icon" /> hillarysousa
-                    </a></li>
-                </ul>
-                <div className="contact__textarea__mail">
-                    <a href="mailto:contato@hillarysousa.com.br" target="_blank" className="contact__textarea__mail__link">
-                        <Image src={Mail} alt="Mail" className="contact__textarea__mail__link__icon" /> contato@hillarysousa.com.br
-                    </a>
-                </div>
-            </div>
-            <div className="contact__footer">
-                <p>{t("t:contact:footer")}{currentYear}</p>
-            </div>
-        </section>
-    )
+  return (
+    <section id="contact" className="contact">
+      <div className="contact__textarea">
+        <h2 className="contact__textarea__title">{t("t:contact:title")}</h2>
+        <ul className="contact__textarea__socials">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/hillarysousa/"
+              target="_blank"
+              className="contact__textarea__socials__link"
+            >
+              <Image
+                src={LinkedIn}
+                alt="LinkedIn"
+                className="contact__textarea__socials__link__icon"
+              />{" "}
+              hillarysousa
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/hillarysousa"
+              target="_blank"
+              className="contact__textarea__socials__link"
+            >
+              <Image
+                src={GitHub}
+                alt="GitHub"
+                className="contact__textarea__socials__link__icon"
+              />{" "}
+              hillarysousa
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.behance.net/hillarysousa"
+              target="_blank"
+              className="contact__textarea__socials__link"
+            >
+              <Image
+                src={Behance}
+                alt="Behance"
+                className="contact__textarea__socials__link__icon"
+              />{" "}
+              hillarysousa
+            </a>
+          </li>
+        </ul>
+        <div className="contact__textarea__mail">
+          <a
+            href="mailto:contato@hillarysousa.com.br"
+            target="_blank"
+            className="contact__textarea__mail__link"
+          >
+            <Image
+              src={Mail}
+              alt="Mail"
+              className="contact__textarea__mail__link__icon"
+            />{" "}
+            contato@hillarysousa.com.br
+          </a>
+        </div>
+      </div>
+      <div className="contact__footer">
+        <p>
+          {t("t:contact:footer")}
+          {currentYear}
+        </p>
+      </div>
+    </section>
+  );
 }
